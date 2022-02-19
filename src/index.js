6eaccfcead616a0a25129d7ab454c0142299e933
marcchik <markstal2002@gmail.com>
2022-02-19 20:53:33 +0300

    Клиент для API

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
 
-const getResource = async (url) => {
-    const res = await fetch(url);
 
-    if (!res.ok) {
-        throw new Error(`Could not fetch ${url}` +
-            `, received ${res.status}`)
+class SwapiService {
+
+    _apiBase = 'https://swapi.dev/api/';
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` +
+                `, received ${res.status}`)
+        }
+
+        return await res.json();
     }
 
-    return await res.json();
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`)
+        return res.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}`)
+    }
+
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`)
+        return res.results;
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}`)
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`)
+        return res.results;
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starships/${id}`)
+    }
 }
 
-getResource('https://swapi.dev/api/people/1/')
-    .then((body) => {
-        console.log(body);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
 
