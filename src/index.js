76c8b1c33e5c0e91c78c3e124534e07fbf728e65
marcchik <markstal2002@gmail.com>
2022-02-19 19:50:08 +0300

    Как работает Fetch API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+}
+
+getResource('https://swapi.dev/api/people/1/')
+    .then((body) => {
+        console.log(body);
+    });
+
